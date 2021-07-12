082494961973d1209fad6249e3f862b720175e03
itshumera <humera0807@gmail.com>
2021-07-12 15:15:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ function flipCard(){
   cardsChosen.push(cardArray[cardId].name);
   this.setAttribute('src', cardArray[cardId].img);
   if(cardsChosen.length == 2){
-    setTimeout(checkForMatch, 1000);
+    setTimeout(checkForMatch, 700);
   }
 }
 
